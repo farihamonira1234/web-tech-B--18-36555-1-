23cf7bcae9802e12fdeea9746d348cbb0ff1d44d
farriha monira <59191475+farihamonira1234@users.noreply.github.com>
2021-04-04 00:25:57 +0600

    Add files via upload

---
+++ Lab Task 7/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index Page</title>
+</head>
+<body style="background-color:powderblue;">
+
+	<h1 style="color:green;">Welcome To Qn Company</h1>
+	<img src="upload/picture.jpg"  class="" width="400" height="100"  >
+	<legend><b><a href="views/login.php">login</a></b></legend> 
+	<legend><b></legend><a href="views/register.php">SignUp</a></b></legend>
+</body>
+</html>
